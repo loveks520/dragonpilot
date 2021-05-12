2d8dc7b26721c9affcf7795327523d922ed54ab9
loveks520 <51947691+loveks520@users.noreply.github.com>
2021-05-12 10:07:14 +0800

    修改 lat_pid
    修改 lat_pid

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -1,4 +1,4 @@
-from selfdrive.controls.lib.pid import PIController
+from selfdrive.controls.lib.pid import P4_PIController
 from selfdrive.controls.lib.drive_helpers import get_steer_max
 from cereal import car
 from cereal import log
@@ -6,10 +6,11 @@ from cereal import log
 
 class LatControlPID():
   def __init__(self, CP):
-    self.pid = PIController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
+    self.pid = P4_PIController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                             (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
-                            k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, neg_limit=-1.0,
-                            sat_limit=CP.steerLimitTimer)
+                            (CP.lateralTuning.pid.kdBP, CP.lateralTuning.pid.kdV),
+                            k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, sat_limit=CP.steerLimitTimer)
+    self.new_kf_tuned = True					
     self.angle_steers_des = 0.
 
   def reset(self):
@@ -33,8 +34,12 @@ class LatControlPID():
       steer_feedforward = self.angle_steers_des   # feedforward desired angle
       if CP.steerControlType == car.CarParams.SteerControlType.torque:
         # TODO: feedforward something based on path_plan.rateSteers
-        steer_feedforward -= path_plan.angleOffset   # subtract the offset, since it does not contribute to resistive torque
-        steer_feedforward *= CS.vEgo**2  # proportional to realigning tire momentum (~ lateral accel)
+        steer_feedforward -= path_plan.angleOffset # subtract the offset, since it does not contribute to resistive torque
+        if self.new_kf_tuned:
+          _c1, _c2, _c3 = 0.35189607550172824, 7.506201251644202, 69.226826411091
+          steer_feedforward *= _c1 * CS.vEgo ** 2 + _c2 * CS.vEgo + _c3
+        else:
+          steer_feedforward *= CS.vEgo ** 2  # proportional to realigning tire momentum (~ lateral accel)
       deadzone = 0.0
 
       check_saturation = (CS.vEgo > 10) and not CS.steeringRateLimited and not CS.steeringPressed
