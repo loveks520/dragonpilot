5b9e7c8656be94d32a7cfaaadfc603dbb344d7d4
loveks520 <51947691+loveks520@users.noreply.github.com>
2021-07-13 16:46:49 +0800

    Update process_config.py

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -12,9 +12,9 @@ procs = [
   # due to qualcomm kernel bugs SIGKILLing camerad sometimes causes page table corruption
   NativeProcess("camerad", "selfdrive/camerad", ["./camerad"], unkillable=True, driverview=True),
   NativeProcess("clocksd", "selfdrive/clocksd", ["./clocksd"]),
-  NativeProcess("dmonitoringmodeld", "selfdrive/modeld", ["./dmonitoringmodeld"], enabled=not JETSON and (not PC or WEBCAM), driverview=True),
+  #NativeProcess("dmonitoringmodeld", "selfdrive/modeld", ["./dmonitoringmodeld"], enabled=not JETSON and (not PC or WEBCAM), driverview=True),
   NativeProcess("logcatd", "selfdrive/logcatd", ["./logcatd"], enabled=not JETSON),
-  NativeProcess("loggerd", "selfdrive/loggerd", ["./loggerd"], enabled=not JETSON),
+  #NativeProcess("loggerd", "selfdrive/loggerd", ["./loggerd"], enabled=not JETSON),
   NativeProcess("modeld", "selfdrive/modeld", ["./modeld"]),
   NativeProcess("proclogd", "selfdrive/proclogd", ["./proclogd"], enabled=not JETSON),
   NativeProcess("sensord", "selfdrive/sensord", ["./sensord"], enabled=not PC, persistent=EON, sigkill=EON),
@@ -24,7 +24,7 @@ procs = [
   PythonProcess("calibrationd", "selfdrive.locationd.calibrationd"),
   PythonProcess("controlsd", "selfdrive.controls.controlsd"),
   PythonProcess("deleter", "selfdrive.loggerd.deleter", persistent=True),
-  PythonProcess("dmonitoringd", "selfdrive.monitoring.dmonitoringd", enabled=not JETSON and (not PC or WEBCAM), driverview=True),
+  #PythonProcess("dmonitoringd", "selfdrive.monitoring.dmonitoringd", enabled=not JETSON and (not PC or WEBCAM), driverview=True),
   PythonProcess("logmessaged", "selfdrive.logmessaged", enabled=not JETSON, persistent=True),
   PythonProcess("pandad", "selfdrive.pandad", persistent=True),
   PythonProcess("paramsd", "selfdrive.locationd.paramsd"),
@@ -35,7 +35,7 @@ procs = [
   PythonProcess("timezoned", "selfdrive.timezoned", enabled=TICI, persistent=True),
   PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC and not JETSON, persistent=True),
   PythonProcess("updated", "selfdrive.updated", enabled=not PC and not JETSON, persistent=True),
-  PythonProcess("uploader", "selfdrive.loggerd.uploader", enabled=not JETSON, persistent=True),
+  #PythonProcess("uploader", "selfdrive.loggerd.uploader", enabled=not JETSON, persistent=True),
   PythonProcess("systemd", "selfdrive.dragonpilot.systemd", persistent=True),
 ]
 
