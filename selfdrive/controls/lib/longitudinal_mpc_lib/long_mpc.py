46028238267ced53403b17ef67e78f198128de10
loveks520 <51947691+loveks520@users.noreply.github.com>
2022-04-08 10:22:50 +0800

    微調

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -342,12 +342,12 @@ class LongitudinalMpc:
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
     elif carstate.distanceLines == 2: # Relaxed
       x_vel = [0.0, 2.788,  5.56,  8.333,  11.11, 13.89, 19.44, 27.78, 41.67]  # velocities
-      y_dist = [1.27, 1.27, 1.29,   1.32,   1.38,  1.38,  1.38,  1.619, 1.8]
+      y_dist = [1.27, 1.27, 1.29,   1.32,   1.38,  1.38,  1.38,  1.559, 1.7]
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
       #self.desired_TF = 1.7
     else:
       x_vel = [0.0, 2.788,  5.56,  8.333,  11.11, 13.89, 19.44, 27.78, 41.67]  # velocities
-      y_dist = [1.37, 1.37, 1.37,  1.4,   1.47,  1.47,  1.698, 1.98,  2.2]
+      y_dist = [1.37, 1.37, 1.37,  1.4,   1.47,  1.47,  1.698, 1.98,  2.0]
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
       #self.desired_TF = T_FOLLOW
 
