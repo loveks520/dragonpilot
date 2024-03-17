e501bc44c9d1e4798cc1963899e50e05c3c31416
loveks520 <51947691+loveks520@users.noreply.github.com>
2024-03-17 23:49:14 +0800

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -433,8 +433,8 @@ class Controls:
 
     # TODO: fix simulator
     if not SIMULATION:
-      if not NOSENSOR and not self.dp_no_gps_ctrl:
-        if not self.sm['liveLocationKalman'].gpsOK and self.sm['liveLocationKalman'].inputsOK and (self.distance_traveled > 1000):
+      #if not NOSENSOR and not self.dp_no_gps_ctrl:
+        #if not self.sm['liveLocationKalman'].gpsOK and self.sm['liveLocationKalman'].inputsOK and (self.distance_traveled > 1000):
           # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
           #self.events.add(EventName.noGps)
 
