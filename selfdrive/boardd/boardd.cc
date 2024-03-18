9ba92d6671e5e19508a798473d93e926d7552040
loveks520 <51947691+loveks520@users.noreply.github.com>
2024-03-18 18:29:10 +0800

    Update boardd.cc

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -682,7 +682,7 @@ static void pigeon_publish_raw(PubMaster &pm, const std::string &dat) {
 void pigeon_thread(Panda *panda) {
   if (!panda->has_gps) return;
   
-  Params().putBool("dp_no_gps_ctrl", !panda->has_gps);
+  #Params().putBool("dp_no_gps_ctrl", !panda->has_gps);
   util::set_thread_name("boardd_pigeon");
 
   PubMaster pm({"ubloxRaw"});
