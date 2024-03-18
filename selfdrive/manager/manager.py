1c0431bf6b1bd03fb08d8deca96fa661410f5399
loveks520 <51947691+loveks520@users.noreply.github.com>
2024-03-18 10:00:17 +0800

    Update manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -162,8 +162,9 @@ def manager_thread() -> None:
     ignore.append("pandad")
   ignore += [x for x in os.getenv("BLOCK", "").split(",") if len(x) > 0]
 
-  #if not params.get_bool("dp_mapd") or params.get_bool("dp_no_gps_ctrl"):
-  ignore += ["mapd", "gpx_uploader", "gpxd"]
+  if not params.get_bool("dp_mapd") or params.get_bool("dp_no_gps_ctrl"):
+    ignore += ["mapd", "gpx_uploader"]
+  ignore += ["gpxd"]
   #if params.get_bool("dp_no_gps_ctrl"):
   ignore += ["ubloxd"]
 
