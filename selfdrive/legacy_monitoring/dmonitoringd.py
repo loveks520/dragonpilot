e14a8aab66c1ddd0b828ede94d0fa0ad37411b7e
loveks520 <51947691+loveks520@users.noreply.github.com>
2024-03-19 17:06:51 +0800

    Update dmonitoringd.py

--- selfdrive/legacy_monitoring/dmonitoringd.py
+++ selfdrive/legacy_monitoring/dmonitoringd.py
@@ -11,7 +11,7 @@ from selfdrive.legacy_monitoring.driver_monitor import DriverStatus
 
 
 def dmonitoringd_thread(sm=None, pm=None):
-  #gc.disable()
+  gc.disable()
   #set_realtime_priority(2)
 
   if pm is None:
