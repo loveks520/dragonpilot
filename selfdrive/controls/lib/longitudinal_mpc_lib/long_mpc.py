5a2d08a8503ec21052d62c41c6062cc0458d1c02
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-03-29 14:27:59 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -84,7 +84,8 @@ def get_stopped_equivalence_factor(v_lead, v_ego):
   v_diff_offset = 0
   if v_lead - v_ego > 0:
     v_diff_offset = (v_lead - v_ego) * 1.2
-    v_diff_offset = np.clip(v_diff_offset, 0, STOP_DISTANCE / 2)
+    v_diff_offset = np.clip(v_diff_offset, 0, 2.0)
+    #v_diff_offset = np.clip(v_diff_offset, 0, STOP_DISTANCE / 2)
     # 當自車時速超過 10 m/s (約 36 km/h) 後，這個補償會線性消失
     v_diff_offset = np.maximum(v_diff_offset * ((10 - v_ego)/10), 0)
     
