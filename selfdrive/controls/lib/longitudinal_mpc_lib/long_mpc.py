42f88b55b6d392aff01a10346b83be6b0599bc11
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-03-29 14:39:16 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -62,7 +62,7 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 0.85
   elif personality==log.LongitudinalPersonality.standard:
-    return 0.75
+    return 0.63
   elif personality==log.LongitudinalPersonality.aggressive:
     return 0.3
   else:
@@ -73,7 +73,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.5
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.4
+    return 1.35
   elif personality==log.LongitudinalPersonality.aggressive:
     return 0.9
   else:
