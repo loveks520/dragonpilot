ffec5e870e1e3b0119e503f80b51defbf7dabbc3
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-03-30 09:52:04 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -60,10 +60,10 @@ def get_coast_accel(pitch):
   return np.sin(pitch) * -5.65 - 0.3  # fitted from data using xx/projects/allow_throttle/compute_coast_accel.py
   
 def get_min_accel_toyota(v_ego):
-  return interp(v_ego, A_CRUISE_MIN_BP_TOYOTA, A_CRUISE_MIN_VALS_TOYOTA)
+  return np.interp(v_ego, A_CRUISE_MIN_BP_TOYOTA, A_CRUISE_MIN_VALS_TOYOTA)
 
 def get_max_accel_toyota(v_ego):
-  return interp(v_ego, A_CRUISE_MAX_BP_TOYOTA, A_CRUISE_MAX_VALS_TOYOTA)
+  return np.interp(v_ego, A_CRUISE_MAX_BP_TOYOTA, A_CRUISE_MAX_VALS_TOYOTA)
 
 def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
   """
