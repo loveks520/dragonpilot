b5dab0c27d980b10f7eff8c5c1e49f7b61a0c07f
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-04-01 18:51:29 +0800

    add toyota

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -157,7 +157,8 @@ class LongitudinalPlanner:
     prev_accel_constraint = not (reset_state or sm['carState'].standstill)
 
     if mode == 'acc':
-      accel_clip = [ACCEL_MIN, get_max_accel(v_ego)]
+      #accel_clip = [ACCEL_MIN, get_max_accel(v_ego)]
+      accel_clip = [get_min_accel_toyota(v_ego), get_max_accel_toyota(v_ego)]
       steer_angle_without_offset = sm['carState'].steeringAngleDeg - sm['liveParameters'].angleOffsetDeg
       accel_clip = limit_accel_in_turns(v_ego, steer_angle_without_offset, accel_clip, self.CP)
     else:
