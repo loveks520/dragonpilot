73e8a679b78e9925daceae01c3e8bb8a7e7ad090
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-04-03 09:09:32 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -53,8 +53,8 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
-COMFORT_BRAKE = 2.2
-STOP_DISTANCE = 5.3
+COMFORT_BRAKE = 2.3
+STOP_DISTANCE = 5.0
 CRUISE_MIN_ACCEL = -1.2
 CRUISE_MAX_ACCEL = 1.6
 
@@ -73,7 +73,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.55
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.35
+    return 1.41
   elif personality==log.LongitudinalPersonality.aggressive:
     return 0.85
   else:
