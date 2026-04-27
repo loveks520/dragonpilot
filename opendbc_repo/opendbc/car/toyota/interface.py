06da23d8eaa7b3f34c488a4def550dbbba011da8
loveks520 <51947691+loveks520@users.noreply.github.com>
2026-04-27 08:11:32 +0800

    更新 interface.py

--- opendbc_repo/opendbc/car/toyota/interface.py
+++ opendbc_repo/opendbc/car/toyota/interface.py
@@ -78,8 +78,8 @@ class CarInterface(CarInterfaceBase):
     if candidate == CAR.TOYOTA_PRIUS:
       stop_and_go = True
       ret.steerActuatorDelay = 0.2
-      ret.steerRatio = 14.8
-      ret.tireStiffnessFactor = 0.85
+      ret.steerRatio = 14.0
+      ret.tireStiffnessFactor = 0.95
       
       #ret.steerActuatorDelay = 0.13
       #ret.steerRatio = 15.7
